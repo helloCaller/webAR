5e8f1e054642073322df76af60eafba4c09dcd93
Luke Garwood <luke_gar@hotmail.com>
2020-07-13 01:04:40 -0400

    adding new file path

--- video.js
+++ video.js
@@ -1,23 +1,8 @@
-AFRAME.registerComponent('vidhandler', {
-  // ...
-  init: function () {
-      this.toggle = false;
-      this.vid = document.querySelector("#videoID")
-      this.vid.pause();
-      var texture = new THREE.VideoTexture( this.vid );
-      texture.minFilter = THREE.LinearFilter;
-      texture.magFilter = THREE.LinearFilter;
-      texture.format = THREE.RGBFormat;
-  },
-  tick: function () {
-      if (this.el.object3D.visible == true) {
-          if (!this.toggle) {
-              this.toggle = true;
-              this.vid.play();
-          }
-      } else {
-          this.toggle = false;
-          this.vid.play();
-      }
-  }
-});
+var video = document.getElementByID("videoID");
+
+async function startPlaying(){
+  await video.load();
+  video.play();
+}
+startPlaying();
+
