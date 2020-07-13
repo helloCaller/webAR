cb87084eda194ecee06c1d9eb1912554b70d8ead
Luke Garwood <luke_gar@hotmail.com>
2020-07-13 01:15:54 -0400

    updating file paths

--- video.js
+++ video.js
@@ -1,8 +1,23 @@
-var video = document.getElementByID("videoID");
-
-async function startPlaying(){
-  await video.load();
-  video.play();
-}
-startPlaying();
-
+AFRAME.registerComponent('vidhandler', {
+  // ...
+  init: function () {
+      this.toggle = false;
+      this.vid = document.querySelector("#videoID")
+      this.vid.pause();
+      var texture = new THREE.VideoTexture( this.vid );
+      texture.minFilter = THREE.LinearFilter;
+      texture.magFilter = THREE.LinearFilter;
+      texture.format = THREE.RGBFormat;
+  },
+  tick: function () {
+      if (this.el.object3D.visible == true) {
+          if (!this.toggle) {
+              this.toggle = true;
+              this.vid.play();
+          }
+      } else {
+          this.toggle = false;
+          this.vid.play();
+      }
+  }
+});
